f7abdb4dfd58f2e01b7acc45a9fba238e091c92b
Loic Correvon <loic.correvon@eeproperty.com>
2025-11-14 10:45:42 +0100

    Stick to my own standards...

--- 2025/02/pt1.sh
+++ 2025/02/pt1.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
+
 source "$(dirname "$0")/../common.sh"
 
 INPUT=${1:-example-input.txt}
 if [[ ! -f "${INPUT}" ]]; then
-    echo "Input file not found: ${INPUT}"
-    exit 1
+    fatal "Input file not found: ${INPUT}"
 fi
 
 # Read input data into REPORTS array
@@ -16,5 +17,5 @@ done < "${INPUT}"
 nb_reports=${#REPORTS[@]}
 for ((i = 0; i < nb_reports; i++)); do
     report=${REPORTS[i]}
-    echo "Repport $i : $report"
+    debug "Repport $i : $report"
 done
