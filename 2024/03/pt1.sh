bb63cbab29d89a3a367647428cbe1a1fead0abc3
Loic Correvon <loic.correvon@eeproperty.com>
2025-11-14 15:53:43 +0100

    finish day03, part 1

---
+++ 2024/03/pt1.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+source "$(dirname "$0")/../common.sh"
+
+INPUT=${1:-example-input.txt}
+if [[ ! -f "${INPUT}" ]]; then
+    fatal "Input file not found: ${INPUT}"
+fi
+
+# Read input data into one line (INSTRUCTIONS)
+INSTRUCTIONS=""
+while IFS= read -r line; do
+    INSTRUCTIONS="${INSTRUCTIONS}${line}"
+done < "${INPUT}"
+
+# Valid input example : mul(X,Y) # X and Y can be 3 digits max
+mul_regex="mul\(([0-9]{1,3}),([0-9]{1,3})\)"
+accumulator=0
+while [[ ${INSTRUCTIONS} =~ ${mul_regex} ]]; do
+    match=${BASH_REMATCH[0]}
+    lhs=${BASH_REMATCH[1]}
+    rhs=${BASH_REMATCH[2]}
+    debug "Match: ${match} / ${lhs} * ${rhs}"
+
+    result=$((lhs * rhs))
+    ((accumulator+=result))
+
+    INSTRUCTIONS=${INSTRUCTIONS/"${match}"/} # Remove current match from INSTRUCTIONS and try again
+done
+
+debug "-------------------------"
+echo "Total : ${accumulator}"
