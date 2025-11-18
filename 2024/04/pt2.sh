c1854b53ca43583d5f9aba735b7b8d207ed20f6e
Loic Correvon <loic.correvon@eeproperty.com>
2025-11-18 22:24:48 +0100

    finish day04, part 2

--- 2024/04/pt2.sh
+++ 2024/04/pt2.sh
@@ -18,36 +18,65 @@ while IFS= read -r line; do
     ((nb_rows++))
 done < "${PUZZLE_INPUT_FILE}"
 
-find_xmas() {
-    local starting_pos=${1:-}
-    local dx=${2:-}
-    local dy=${3:-}
+# Check in a 3x3 square, coordinate going from starting_pos.x, starting_pos.y to starting_pos.x+2, starting_pos.y+2
+function find_xmas() {
+    local start_x=${1:-}
+    local start_y=${2:-}
 
-    [[ -z ${starting_pos} ]] && fatal "starting_pos is empty" 10
-    [[ ${dx} =~ \-?[0-9]+ ]] || fatal "dx must be an integer" 10
-    [[ ${dy} =~ \-?[0-9]+ ]] || fatal "dy must be an integer" 10
+    [[ ${start_x} =~ [0-9]+ ]] || fatal "start_x must be an integer" 11
+    [[ ${start_y} =~ [0-9]+ ]] || fatal "start_y must be an integer" 11
 
-    local next_x next_y
-    IFS=, read -r next_x next_y <<< ${starting_pos}
-    [[ ${next_x} =~ [0-9]+ ]] || fatal "next_x must be an integer" 11
-    [[ ${next_y} =~ [0-9]+ ]] || fatal "next_y must be an integer" 11
+    # Top left
+    local key="${start_x},${start_y}"
+    local top_left="${grid[$key]}"
+    debug "Getting 3x3 square, top left : ${start_x},${start_y} = ${top_left}"
 
-    debug "next_x = $next_x / next_y = $next_y / dx = $dx / dy = $dy"
+    local opposite_side
+    case "${top_left}" in
+        M)  opposite_side="S" ;;
+        S)  opposite_side="M" ;;
+        *)  debug "  Top-left is not even M or S (is ${top_left})"
+            return 1;;
+    esac
 
-    local word="${grid[$starting_pos]}"
-    local i next_char key
-    for((i = 0; i < 3; i++)); do
-        ((next_x += dx))
-        ((next_y += dy))
-        key="${next_x},${next_y}"
-        next_char="${grid[$key]:-Z}" # Replace out-of-bounds with invalid char
+    # Center
+    local next_x=$((start_x + 1))
+    local next_y=$((start_y + 1))
+    key="${next_x},${next_y}"
+    local center_char="${grid[$key]:-Z}"
+    if [[ "${center_char}" != "A" ]]; then
+        debug "  Center is not even A (is ${center_char})"
+        return 1
+    fi
 
-        word="${word}${next_char}"
-        if [[ ! "XMAS" =~ ${word}.* ]]; then
-            debug "  ${word} is not XMAS"
-            return 1
-        fi
-    done
+    # Top right
+    next_x=$((start_x + 2))
+    next_y=$((start_y))
+    key="${next_x},${next_y}"
+    local top_right="${grid[$key]}"
+
+    # Bottom left
+    next_x=$((start_x))
+    next_y=$((start_y + 2))
+    key="${next_x},${next_y}"
+    local bottom_left="${grid[$key]}"
+    if [[ ("${top_left}" == "${top_right}" && "${bottom_left}" == "${opposite_side}") || \
+          ("${top_left}" == "${bottom_left}" && "${top_right}" == "${opposite_side}") ]]; then
+        debug "  Checking top-left vs other corner : ok for now"
+    else
+        debug "  No matching top-left corner. ${top_left} vs ${top_right} and ${bottom_left}"
+        return 1
+    fi
+
+    # Bottom right
+    next_x=$((start_x + 2))
+    next_y=$((start_y + 2))
+    key="${next_x},${next_y}"
+    local bottom_right="${grid[$key]}"
+    if [[ "${bottom_right}" != "${opposite_side}" ]]; then
+        debug "  Bottom-right is not opposite. ${bottom_right} vs ${opposite_side}"
+        return 1
+    fi
 
     debug "  Found XMAS spirit !"
     return 0
@@ -55,23 +84,12 @@ find_xmas() {
 
 # Main logic
 xmas_found=0
-for((y = 0; y < nb_rows; y++)); do
-    for((x = 0; x < nb_columns; x++)); do
-        key="${x},${y}"
-        char="${grid[$key]}"
-        # echo -n "${char}" >&2
-        if [[ "${char}" == "X" ]]; then
-            debug
-            # Check the 8 cardinal directions
-            for((dx = -1; dx <= 1; dx++)); do
-                for((dy = -1; dy <= 1; dy++)); do
-                    ((dx == 0 && dy == 0)) && continue
-                    find_xmas ${key} ${dx} ${dy} && ((xmas_found++))
-                done
-            done
-        fi
+for((y = 0; y < nb_rows-2; y++)); do
+    for((x = 0; x < nb_columns-2; x++)); do
+        debug
+        find_xmas ${x} ${y} && ((xmas_found++))
     done
 done
 
 debug "-------------------------"
-echo "XMAS found : ${xmas_found}"
+echo "X-MAS found : ${xmas_found}"
