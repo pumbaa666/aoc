a353c1944bd7ea2361f23de32ec8f2886d849962
Loic Correvon <loic.correvon@eeproperty.com>
2025-11-23 15:15:08 +0100

    Boostrap day07 part 1

---
+++ 2024/07/pt1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source "$(dirname "$0")/../common.sh"
+
+# Read input data
+while IFS= read -r line; do
+    read -r array <<< ${line}
+done < "${PUZZLE_INPUT_FILE}"
+
+# Main logic
+accumulator=0
+
+debug "-------------------------"
+echo "Sum of valid equations : ${accumulator}"
