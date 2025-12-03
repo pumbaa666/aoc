9d365349486f6a3dc2eec3d7c47c3bc1aef74ba1
Loic Correvon <loic.correvon@eeproperty.com>
2025-12-03 18:42:59 +0100

    Start 2025 !

---
+++ 2025/check-all-challenges.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+day_min="${1:-01}"
+day_max="${2:-31}"
+
+input_types=("example-input" "real-input")
+for day in $(find . -mindepth 1 -maxdepth 1 -type d | sort); do
+    day_str=$(realpath --relative-to="." ${day})
+
+    # Skip irrelevant days
+    [[ "$day_str" < "$day_min" || "$day_str" > "$day_max" ]] && continue
+
+    echo "Day ${day_str}"
+    for((part = 1; part <= 2; part++)); do
+        echo -n "  Part ${part}"
+
+        for input_type in ${input_types[@]}; do
+            echo -n " / ${input_type}"
+            result=$(bash "${day}/pt${part}.sh" --puzzle="${day}/inputs/${input_type}.pt${part}")
+            expected_result=$(cat "${day}/results/${input_type}-result.pt${part}")
+
+            [[ "${result}" != "${expected_result}" ]] && echo -n " ❌" || echo -n " ✅"
+        done
+        echo ""
+    done
+    echo ""
+done
